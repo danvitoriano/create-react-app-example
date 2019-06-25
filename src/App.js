fbd74e03826e0d2da838f3aea9f45eee142dcede
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-06-24 22:49:52 -0300

    Inicial react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
+  //react aceita apenas um elemento do dom no retorno
+  // <> </> engloba elementos na versão mais nova
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
+}*/
+/*
+function App() {
+  //react aceita apenas um elemento do dom no retorno
+  // <> </> engloba elementos na versão mais nova
+  return (
+      <>
+        <p>lalala</p>
+        <h4>kkkk</h4>
+      </>
+  );
+}*/
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {name:"danilo"};
+  }
+  update(){ return null; }
+  render(){
+    return <p>Ok</p>;
+  }
 }
+
 
 export default App;
